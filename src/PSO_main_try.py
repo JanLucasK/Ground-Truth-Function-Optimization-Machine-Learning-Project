fd7b113dd32ca6267f198106bee141206e7949cb
Denalii <lukahipp@googlemail.com>
2023-08-31 15:50:46 +0200

    PSO optimized

--- src/PSO_main_try.py
+++ src/PSO_main_try.py
@@ -1,24 +1,40 @@
 from bin.PSO_optimizer import PSO_optimizer  # Import the PSO_optimizer class
-import bbobtorch
+import numpy as np
 
 def main():
-    # Define the BBOB function you want to use
-    bbob_function = bbobtorch.create_f01(2, seed=42)  # Replace with the desired BBOB function
+    input_bounds=[(-5.0,5.0), (-5.0,5.0)]
+    swarmsize = 50  # Number of particles each swarm
+    niter=20 # Number of iterations
 
-    # Create an instance of the PSO_optimizer class and run the optimization
-    model_path = 'models/v1/training_v1_f01_3.pth'
-    input_bounds = [(-5.0, 5.0), (-5.0, 5.0)]
-    num_particles = 50  # Number of particles each swarm
-
-    optimizer = PSO_optimizer(model_path, input_bounds=input_bounds, bbob_function=bbob_function, num_particles=num_particles)
-    initial_guess = [0.0, 0.0]
-    niter = 20  # Number of iterations
-
-    result_nn, result_bbob, fig = optimizer.optimize(initial_guess, niter, num_particles)
-
-    # Adjust the path where the animation GIF will be saved
-    anim_filename = "/home/luka/Documents/test_01_pso_animation.gif"
-    fig.savefig(anim_filename)  # Save the final visualization
+    models = [
+        ["f_01", "models/v3/training_v3_f01_4.pth"],
+        ["f_03","models/v3/training_v3_f03_5.pth"],
+        ["f_24", "models/v3/training_v3_f24_4.pth"]
+        ]
+    pso_opt = PSO_optimizer(input_bounds=input_bounds)
+    for model in models:
+        seed = 0
+        for _ in range(3):
+            name = model[0]+"_"+str(seed)
+            result_nn, result_bbob, fig = pso_opt.optimize(model_path= model[1], function =model[0],  
+                                                               swarmsize=swarmsize, 
+                                                               niter=niter, seed= seed, 
+                                                               save_image=True, image_name=name)
+            print(model[0]+"_"+str(seed))
+            print(result_nn.x)
+            print(result_bbob.x)
+            #distance = basehop_opt.calc_distance(point_a=result_nn.x, point_b=result_bbob.x)
+            distance = np.linalg.norm(result_nn.x-result_bbob.x) 
+            print(distance)
+            with open('pso_results.txt', "a") as f:
+                f.write(f"{name}")
+                f.write(f"\n")
+                f.write(f"neural-net optium: {result_nn.x}, ground-thruth optimum: {result_bbob.x}")
+                f.write(f"\n")
+                f.write(f"distance: {distance}")
+                f.write(f"\n")
+                f.write(f"\n")
+            seed+=1
 
 if __name__ == "__main__":
     main()
--- src/bin/PSO_optimizer.py
+++ src/bin/PSO_optimizer.py
@@ -8,15 +8,9 @@ import matplotlib.animation as animation
 
 
 class PSO_optimizer():
-    def __init__(self, model_path, input_bounds=None, bbob_function=None, num_particles=50) -> None:   
-        self.model = self.load_model(model_path)
+    def __init__(self, input_bounds=None, swarmsize=50) -> None:   
         self.input_bounds = input_bounds
-        self.model.eval()
-        self.bbob = bbob_function
-        self.num_particles = num_particles
-        self.bbob_path = pd.DataFrame(columns=["x1", "x2", "y"])
-        self.model_path = pd.DataFrame(columns=["x1", "x2", "y"])
-        self.path = []
+        self.swarmsize = swarmsize
         self.particle_history = []
    
     def load_model(self, path):
@@ -40,20 +34,38 @@ class PSO_optimizer():
         self.model_path.loc[len(self.model_path)] = model_point
         return y_scalar
     
-    def optimize(self, initial_guess, niter=100, num_particles=50):
-        # Use PSO optimization instead of Basinhopping
-        result_nn, _ = pso(self.call_nn, swarmsize=num_particles, maxiter=niter, minstep=1e-5, lb= [-5,-5], ub=[5, 5])
-        result_bbob, _ = pso(self.call_bbob, swarmsize=num_particles, maxiter=niter, minstep=1e-5, lb= [-5,-5], ub=[5, 5])
+    def optimize(self, model_path, function='f_01', niter=100, swarmsize=50, image_name = "Default", save_image=False, seed=42):
+        self.model = self.load_model(model_path)
+        self.model.eval()
+        if function == "f_01":
+            self.bbob = bbobtorch.create_f01(2, seed=42)
+        elif function=="f_03":
+            self.bbob = bbobtorch.create_f03(2, seed=42)
+        else:
+            self.bbob = bbobtorch.create_f24(2, seed=42)
         
+        self.bbob_path = pd.DataFrame(columns=["x1", "x2", "y"])
+        self.model_path = pd.DataFrame(columns=["x1", "x2", "y"])
+        self.path = []
+
+        self.save_image = save_image
+        self.image_name = image_name
+
+        # Use PSO optimizer
+        result_nn, _ = pso(self.call_nn, swarmsize=swarmsize, maxiter=niter, minstep=1e-5, lb = [-5,-5], ub=[5,5])
         model_path = np.array(self.path)
         self.path = []
 
+        result_bbob, _ = pso(self.call_bbob, swarmsize=swarmsize, maxiter=niter, minstep=1e-5, lb = [-5,-5], ub=[5,5])
+        bbob_path = np.array(self.path)
+        
         # fig = self.visualize_paths(result_bbob, result_nn)
-        fig = self.visualize_paths(self.bbob_path.values, self.model_path.values, result_bbob, result_nn, num_particles)
+        fig = self.visualize_paths(self.bbob_path, self.model_path, result_bbob, result_nn, swarmsize)
 
         return result_nn, result_bbob, fig
     
-    def visualize_paths(self, path_bbob, path_model, result_bbob, result_model, num_particles):
+    
+    def visualize_paths(self, path_bbob, path_model, result_bbob, result_model, swarmsize):
         x_grid, y_grid = np.meshgrid(np.arange(-5, 5.01, 0.01), np.arange(-5, 5.01, 0.01))
 
         # Convert the scaled grid data to PyTorch tensor
@@ -66,35 +78,34 @@ class PSO_optimizer():
         with torch.no_grad():
             predictions = self.model(grid_data_tensor).numpy().reshape(x_grid.shape)
 
-        function_values = self._get_gt_function(x_grid, y_grid)
+        function_values_bbob = self._get_gt_function(x_grid, y_grid)
 
         fig, axes = plt.subplots(1, 2, figsize=(16, 8))
 
-        def animate(frame, num_particles):
-            axes[0].clear()
-            axes[1].clear()
+        ax1 = axes[0]  # Define ax1 for the left plot prediction
+        ax2 = axes[1]  # Define ax2 for the right plot bbob
 
-            ax1 = axes[0]
-            ax2 = axes[1]
+        ax1.contourf(x_grid, y_grid, predictions, levels=100, cmap='viridis')
+        ax2.contourf(x_grid, y_grid, function_values_bbob, levels=100, cmap='viridis')
 
-            ax1.contourf(x_grid, y_grid, predictions, levels=100, cmap='viridis')
-            ax2.contourf(x_grid, y_grid, function_values, levels=100, cmap='viridis')
+        for ax, path in zip([ax1, ax2], [path_model, path_bbob]):
+            for j in range(0, len(path), swarmsize):
+                best_particle = path[j:j+swarmsize][np.argmin(path[j:j+swarmsize][:, 2])]
+                ax.scatter(path[j:j+swarmsize, 0], path[j:j+swarmsize, 1], c='red', s=10, marker='o', alpha=0.5)
+                ax.scatter(best_particle[0], best_particle[1], c='green', s=100, marker='o')  # Mark gBest with a cross
 
-            for i, (ax, path) in enumerate(zip([ax1, ax2], [path_model, path_bbob])):
-                start = frame * num_particles
-                end = start + num_particles
+            if ax is ax1:  # Plot result for the left subplot
+                ax.scatter(x=result_model[0], y=result_model[1], c='black', s=250, marker='x')
+            else:  # Plot result for the right subplot
+                ax.scatter(x=result_bbob[0], y=result_bbob[1], c='black', s=250, marker='x')
 
-                ax.scatter(path[start:end, 0], path[start:end, 1], c='red', s=10, marker='o', alpha=0.5)
+        ax1.set_title("Model Predictions")
+        ax2.set_title("BBOB Function")
 
-                axes[0].scatter(x=result_model[0], y=result_model[1], c='black', s=250, marker='x')
-                axes[1].scatter(x=result_bbob[0], y=result_bbob[1], c='black', s=250, marker='x')
-
-        anim = animation.FuncAnimation(fig, animate, frames=len(path_model) // num_particles, interval=200, repeat=False)
-
-        # Save the animation
-        # anim_filename = "/home/luka/Documents/24_pso_animation.gif"  # Specify the filename
-        # anim.save(anim_filename, writer='pillow', fps=2, dpi=150)
-
+        plt.tight_layout()
+        if self.save_image:
+            plt.savefig('24_swarm_iterations.png')  # Save the plot as a PNG image
+        
         plt.show()
         return plt
     
@@ -107,6 +118,7 @@ class PSO_optimizer():
                     penalty += 1e5  # Adjust the penalty value based on your specific needs
         
         x_tensor = torch.tensor(x, dtype=torch.float32)
+        x_tensor = x_tensor.view(1,-1)
         y = self.bbob(x_tensor)
         y_scalar = torch.sum(y).item() + penalty
         
